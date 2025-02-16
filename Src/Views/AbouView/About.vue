9ed91eb856b90e23f62d8f6d8ddf48022cbfaf99
zainab76810510 <zainab76810510@gmail.com>
2025-02-16 23:24:38 +0300

    Delete Src directory

--- Src/Views/AbouView/About.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import HeroSection from './Components/HeroSection.vue'
-import SectionTwo from './Components/SectionTwo.vue';
-</script>
-<template>
-    <hero-section />
-    <section-two />
-</template>
